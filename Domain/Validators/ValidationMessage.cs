64b8492d835c4a33f0b8de920a4e58dd6f70bf48
djdizel <158520404+djdizel@users.noreply.github.com>
2024-11-25 13:43:09 +0200

    123

--- Domain/Validators/ValidationMessage.cs
+++ Domain/Validators/ValidationMessage.cs
@@ -5,16 +5,17 @@ public static class ValidationMessage
     public static string NotNull = "{PropertyName} не может быть NULL}";
     public static string NotEmpty = "{PropertyName} не может быть empty}";
     public static string WrongLength = "{PropertyName} должен быть от {min} до {max} символов}";
-    public const string RequiredField = "Field is required.";
-    public const string LengthField = "Field must be between 2 and 150 characters.";
-    public const string OnlyLettersDigitsAndSpaces = "Field must contain only letters, digits, and spaces.";
-    public const string OnlyLettersSpacesAndDashes = "Field is required.";
-    public const string ExactLengthField = "Field is required.";
-    public const string OnlyUppercaseLetters = "Field is required.";
-    public const string ValidCountryCode = "Field is required.";
-    public const string OnlyLettersAndSpaces = "Field must contain only letters, digits, and spaces.";
-    public const string OnlyLettersDigitsSpacesAndDashes = "Field must contain only letters, digits, and spaces.";
-    public const string OnlyLettersDigitsAndDashes = "Field must contain only letters, digits, and spaces.";
-    public const string PositiveNumber = "Field must contain only letters, digits, and spaces.";
+    public const string RequiredField = "{PropertyName} обязательно для заполнения."; 
+    public const string LengthField = "Длина {PropertyName} должна быть от {min} до {max} символов."; 
+    public const string OnlyLettersDigitsAndSpaces = "{PropertyName} может содержать только буквы, цифры и пробелы."; 
+    public const string OnlyLettersSpacesAndDashes = "{PropertyName} может содержать только буквы, пробелы и дефисы."; 
+    public const string ExactLengthField = "{PropertyName} должно содержать ровно {length} символов."; 
+    public const string OnlyUppercaseLetters = "{PropertyName} может содержать только заглавные буквы."; 
+    public const string ValidCountryCode = "{PropertyName} должно содержать корректный код страны."; 
+    public const string OnlyLettersAndSpaces = "{PropertyName} может содержать только буквы и пробелы."; 
+    public const string OnlyLettersDigitsSpacesAndDashes = "{PropertyName} может содержать только буквы, цифры, пробелы и дефисы."; 
+    public const string OnlyLettersDigitsAndDashes = "{PropertyName} может содержать только буквы, цифры и дефисы."; 
+    public const string PositiveNumber = "{PropertyName} должно содержать положительное число."; 
+
 
 }
